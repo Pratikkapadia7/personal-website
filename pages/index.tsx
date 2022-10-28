360c928e168630baf178f8b224822200b2cb18ab
Pratik Kapadia <pkkpratikk@gmail.com>
2022-10-28 11:25:50 +0530

    Submodules initialization

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,10 @@ import NavigationBar from "../src/components/global/NavigationBar";
 import CoverTitle from "../src/components/home/CoverTitle";
 import MainBlogComponent from "../src/components/home/Blogs/MainBlogComponent";
 import FooterComponent from "../src/components/global/FooterComponent";
+import content from "content/data/content";
 
 const Home: NextPage = () => {
+  console.log("CONTENT==>>", content);
   return (
     <Box>
       <NavigationBar />
